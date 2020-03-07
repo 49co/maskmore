a7fad10d40cbb02648f6040c2f5775d086121c61
heetak chung <HeeTop@users.noreply.github.com>
2020-03-07 22:49:58 +0900

    [BACK-12] sale field 추가 (#15)
    feat: sale field 추가

--- maskmore/store/models.py
+++ maskmore/store/models.py
@@ -12,6 +12,11 @@ class MaskProduct(models.Model):
         ('KF80', 'KF80'),
         ('ETC', 'ETC'),
     )
+
+    SALE = (
+        ('on sale', 'on sale'),
+        ('sold out', 'sold out'),
+    )
     mask_id        = models.AutoField(primary_key=True)
     title          = models.CharField(max_length= 100)
     description    = models.TextField(blank=True)
@@ -24,11 +29,12 @@ class MaskProduct(models.Model):
     published_date = models.DateTimeField(auto_now_add= True, blank= True)
     email          = models.EmailField(max_length=254, blank= True)
     update_time    = models.CharField(blank=True, max_length=400)
-    
+    sale           = models.CharField(max_length=10 ,choices=SALE, default='sold out')
+
     def __str__(self):
         return self.title
 
 class MaskProductSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = MaskProduct
-        fields = ['title', 'description', 'origin_url', 'image', 'package_price', 'price', 'category', 'email', 'update_time', 'is_visible']
+        fields = ['title', 'description', 'origin_url', 'image', 'package_price', 'price', 'category', 'email', 'update_time', 'sale', 'is_visible']

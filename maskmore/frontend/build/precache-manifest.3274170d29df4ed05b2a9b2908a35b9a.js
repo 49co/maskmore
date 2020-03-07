c8dd15a02721a7d1b9ca6fc7b960351bccccda3e
Taehun Im <95imtae@gmail.com>
2020-03-07 23:32:32 +0900

    [FE-05] 상품 리스트 보여주기 (#16)
    * feat: axios 추가
    
    * refactor: manifest.json 주석 처리
    
    * feat: 상품 리스트 보여주기

---
+++ maskmore/frontend/build/precache-manifest.3274170d29df4ed05b2a9b2908a35b9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41ca6aa21e28afce61e3a4511721b5da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab00fc6f8536d82965b9",
+    "url": "/static/css/main.6b6d289a.chunk.css"
+  },
+  {
+    "revision": "8b37ab9fbaa8c8f32bcb",
+    "url": "/static/js/2.0ab63f25.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.0ab63f25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab00fc6f8536d82965b9",
+    "url": "/static/js/main.9642ee87.chunk.js"
+  },
+  {
+    "revision": "7a008b5c3a6c3fefae51",
+    "url": "/static/js/runtime-main.18445cb3.js"
+  }
+]);
--- maskmore/frontend/src/components/Product.js
+++ maskmore/frontend/src/components/Product.js
@@ -3,19 +3,21 @@ import styles from '../scss/Product.module.scss';
 import siren from '../img/siren.png';
 
 const Product = props => {
+  const { mask } = props;
+
   return (
     <li className={styles.li}>
-      <a className={styles.link} href="https://store.musinsa.com/app/product/detail/1260412/0" target="_blank" rel="noopener noreferrer">
-        <img className={styles.img} src="https://image.msscdn.net/images/goods_img/20191231/1260412/1260412_2_500.jpg" alt="마스크 이미지"/>
+      <a className={styles.link} href={mask.origin_url} target="_blank" rel="noopener noreferrer">
+        <img className={styles.img} src={mask.image} alt="마스크 이미지"/>
         <div className={styles.imgLayer}></div>
         <div className={styles.content}>
           <div className={styles.title}>
-            <span className={styles.name}>[임태몰] </span>
-            <span>KF80 마스크 쇼핑몰에서서 정해 놓은 상품명 5개입</span>
+            <span className={styles.name}>[{mask.title}] </span>
+            <span>{mask.description}</span>
           </div>
           <div className={styles.priceContent}>
-            <span className={styles.totalPrice}>17,700원 </span>
-            <span className={styles.price}>(1개 3,760원)</span>
+            <span className={styles.totalPrice}>{mask.package_price.toLocaleString()}원 </span>
+            <span className={styles.price}>(1개 {mask.price.toLocaleString()}원)</span>
           </div>
         </div>
       </a>

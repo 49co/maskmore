11ec8497eeecdee675bc401a076f969da515df04
heetak chung <HeeTop@users.noreply.github.com>
2020-03-07 21:01:32 +0900

    [BACK-11] email, update field 추가 (#13)
    feat: email, update field 추가

--- maskmore/store/models.py
+++ maskmore/store/models.py
@@ -12,21 +12,23 @@ class MaskProduct(models.Model):
         ('KF80', 'KF80'),
         ('ETC', 'ETC'),
     )
-    mask_id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length= 100)
-    description = models.TextField(blank=True)
-    origin_url = models.URLField(blank=True, max_length=400)
-    image = models.FileField(blank=True, upload_to= 'store/masks/')
-    package_price = models.IntegerField(default=0)
-    price = models.IntegerField(default=0)
-    category = models.CharField(max_length=4, choices=CATEGORY, default='ETC')
-    is_visible = models.CharField(max_length=1 ,choices=VISIBLE, default='F')
+    mask_id        = models.AutoField(primary_key=True)
+    title          = models.CharField(max_length= 100)
+    description    = models.TextField(blank=True)
+    origin_url     = models.URLField(blank=True, max_length=400)
+    image          = models.FileField(blank=True, upload_to= 'store/masks/')
+    package_price  = models.IntegerField(default=0)
+    price          = models.IntegerField(default=0)
+    category       = models.CharField(max_length=4, choices=CATEGORY, default='ETC')
+    is_visible     = models.CharField(max_length=1 ,choices=VISIBLE, default='F')
     published_date = models.DateTimeField(auto_now_add= True, blank= True)
-
+    email          = models.EmailField(max_length=254, blank= True)
+    update_time    = models.CharField(blank=True, max_length=400)
+    
     def __str__(self):
         return self.title
 
 class MaskProductSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = MaskProduct
-        fields = ['title', 'description', 'origin_url', 'image', 'package_price', 'price', 'category', 'is_visible']
+        fields = ['title', 'description', 'origin_url', 'image', 'package_price', 'price', 'category', 'email', 'update_time', 'is_visible']

8cb0114c0fb872933660612fceedccd6c1df41b7
heetak chung <HeeTop@users.noreply.github.com>
2020-03-07 20:02:11 +0900

    [BACK-10] csrf 쿠키 추가 (#12)
    feat: csrf 쿠키 추가

--- maskmore/store/views.py
+++ maskmore/store/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework import viewsets
 from .models import MaskProduct, MaskProductSerializer
+from django.views.decorators.csrf import ensure_csrf_cookie
 
-
+@ensure_csrf_cookie
 def index(request):
     return render(request, 'index.html')
 
